943f706784f041ca9febf990038ace1c277831dd
ave <ayvacs@proton.me>
2023-10-06 16:11:27 -0400

    base

--- Program.cs
+++ Program.cs
@@ -1,2 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace nsam;
+
+internal class Program
+{
+    static void GetConsent()
+    {
+        Console.WriteLine("\nI understand that by using this program to modify achievements, I may mess something up and I can only blame myself for the damage I cause. (y/n)");
+        if (Console.ReadLine() != "y")
+        {
+            Console.WriteLine("\nConsent failed.");
+            System.Environment.Exit(1);
+        }
+    }
+
+    static void Main()
+    {
+        Console.WriteLine(" _        _______  _______  _______ ");
+        Console.WriteLine("( (    /|(  ____ \\(  ___  )(       )");
+        Console.WriteLine("|  \\  ( || (    \\/| (   ) || () () |");
+        Console.WriteLine("|   \\ | || (_____ | (___) || || || |");
+        Console.WriteLine("| (\\ \\) |(_____  )|  ___  || |(_)| |");
+        Console.WriteLine("| | \\   |      ) || (   ) || |   | |");
+        Console.WriteLine("| )  \\  |/\\____) || )   ( || )   ( |");
+        Console.WriteLine("|/    )_)\\_______)|/     \\||/     \\|");
+
+        Console.WriteLine("\nPlease enter your Steam login name:");
+        var CurrentSteamUsername = Console.ReadLine();
+        Console.WriteLine("\nPlease enter your Steam password:");
+        var CurrentSteamPassword = Console.ReadLine();
+
+        GetConsent();
+        Console.WriteLine("\nConsented.");
+    }
+}
